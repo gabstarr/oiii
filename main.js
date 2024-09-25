13bff559ef6c2b98a826e30a1750da81b5ab3270
Gabriele <142537944+gabstarr@users.noreply.github.com>
2024-09-25 08:53:07 -0300

    jyhtere

--- main.js
+++ main.js
@@ -9,3 +9,8 @@ console.log(areaQuadrado(6));
 let media = function(numero1,numero2,numero3){
     return (numero1+numero2+numero3)/3;
 }
+let numero3 = parseInt(prompt('digite um numero'))
+let numero2 = parseInt(prompt('digite um numero'))
+let numero1 = parseInt(prompt('digite um numero'))
+
+document.write(media(numero1, numero2, numero3))
