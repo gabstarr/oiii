4642ada2e0d90cb6a5cf3d1ba57d426bbd0c0367
Gabriele <142537944+gabstarr@users.noreply.github.com>
2024-10-02 08:06:26 -0300

    iuythrgfas

--- main.js
+++ main.js
@@ -18,8 +18,8 @@
 let estados =['RS', 'SC','PR','SP','RJ','MG','ES','MT','MS','GO', 'DF','AC','RO', 'AM', 'RR', 'PA', 'AP', 'TO', 'MA', 'PI', 'CE','RN','PB', 'BA', 'FE', 'AL', 'SE',
      ];
 let i=0;
-     while (i<5 ){
+     while (i<estados.length ){
 console.log(i);
-console.log(estados[i]);
+console.log(estados[i])
 i=i+1;
      }
