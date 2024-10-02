05e66540eb6a876faf36a785c0cd7bf014c116fe
Gabriele <142537944+gabstarr@users.noreply.github.com>
2024-10-02 07:41:53 -0300

    HGF

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
-function soma (numero1,numero2){
-    return numero1+numero2;
-}
-console.log(soma(5,2));
-function areaQuadrado(lado){
-    return lado**2;
-}
-console.log(areaQuadrado(6));
-let media = function(numero1,numero2,numero3){
-    return (numero1+numero2+numero3)/3;
-}
-let numero3 = parseInt(prompt('digite um numero'))
-let numero2 = parseInt(prompt('digite um numero'))
-let numero1 = parseInt(prompt('digite um numero'))
+// function soma (numero1,numero2){
+//     return numero1+numero2;
+// }
+// console.log(soma(5,2));
+// function areaQuadrado(lado){
+//     return lado**2;
+// }
+// console.log(areaQuadrado(6));
+// let media = function(numero1,numero2,numero3){
+//     return (numero1+numero2+numero3)/3;
+// }
+// let numero3 = parseInt(prompt('digite um numero'))
+// let numero2 = parseInt(prompt('digite um numero'))
+// let numero1 = parseInt(prompt('digite um numero'))
 
-document.write(media(numero1, numero2, numero3))
+// document.write(media(numero1, numero2, numero3))
+
+let estados =['RS', 'SC','PR','SP','RJ','MG','ES','MT','MS','GO', 'DF','AC','RO', 'AM', 'RR', 'PA', 'AP', 'TO', 'MA', 'PI', 'CE','RN','PB', 'BA', 'FE', 'AL', 'SE',
+     ];
+     
