c3e40a37c3929c26418a40352b49801e7413923b
Gabriele <142537944+gabstarr@users.noreply.github.com>
2024-10-02 08:18:32 -0300

    UTDFhh

--- main.js
+++ main.js
@@ -24,7 +24,8 @@ console.log(estados[i])
 i=i+1;
      }
     
-for(let i=0; i<estados.length;i++){
-console.log(i);
-console.log(estados[i]);
+ufor (let i = 0; i < estados.length; i++) {
+    console.log(i);
+    console.log(estados[i]);
+    
 }
